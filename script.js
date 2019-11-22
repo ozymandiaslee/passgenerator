516e33c1b36385e4e74586dfc9e0f06c9a3f4f68
Ozymandias <raymondlee@kaffeinatenc.com>
2019-11-22 17:11:07 -0500

    First push

---
+++ script.js
@@ -0,0 +1,29 @@
+var passEl = document.querySelector("#password");
+var lengthEl = document.querySelector("#length");
+var charEl = document.querySelector("#char");
+var generateBt = document.querySelector("#generate");
+var charArray = [specialChars, numericChars, uppercaseChars, lowercaseChars];
+
+var specialChars = "!#$%&*+=-/:;<>?";
+var numericChars = "1234567890";
+var uppercaseChars = "QWERTYUIOPASDFGHJKLZXCVBNM";
+var lowercaseChars = "qwertyuiopasdfghjklzxcvbnm";
+
+var specialLength = specialChars.length;
+var numericLength = numericChars.length;
+var uppercaseLength = uppercaseChars.length;
+var lowercaseLength = lowercaseChars.length;
+var arrayLength = charArray.length;
+
+function generateCharset() {
+    var charSet = "";
+    
+}
+
+function generatePassword(length) {
+    var newPass = '';
+    
+    for ( var i = 0; i < length; i++) {
+    if ()
+    }
+}
