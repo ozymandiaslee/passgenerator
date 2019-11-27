7618960ad1a61f7b11aba1c6af36c4f22fc2d211
Ozymandias <raymondlee@kaffeinatenc.com>
2019-11-26 20:03:08 -0500

    Password element updated

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ var passEl = document.querySelector("#password");
 var lengthEl = document.querySelector("#length");
 var charEl = document.querySelector("#char");
 var generateBt = document.querySelector("#generate");
-var length = parseInt(prompt("Length of password?"));
+
 
 var specialChars = "!#$%&*+=-:;<>?";
 var numericChars = "1234567890";
 var uppercaseChars = "QWERTYUIOPASDFGHJKLZXCVBNM";
 var lowercaseChars = "qwertyuiopasdfghjklzxcvbnm";
 
-var specialLength = specialChars.length;
-var numericLength = numericChars.length;
-var uppercaseLength = uppercaseChars.length;
-var lowercaseLength = lowercaseChars.length;
+var specCharCheck = confirm("Use special characters?");
+var numCharCheck = confirm("Use numeric characters?");
+var lowerCaseCheck = confirm("Use lowercase characters?");
+var upperCaseCheck = confirm("Use uppercase characters?");
+
+var length = prompt("Length of password?");
+var characters = "";
 
 function generateCharset() {
-    var specCharCheck = confirm("Use special characters?");
-    var numCharCheck = confirm("Use numeric characters?");
-    var lowerCaseCheck = confirm("Use lowercase characters?");
-    var upperCaseCheck = confirm("Use uppercase characters?");
+    
     var charCheckArray = [specCharCheck, numCharCheck, lowerCaseCheck, upperCaseCheck, specialChars, numericChars, uppercaseChars, lowercaseChars];
     var charSet = "";
     for ( var i = 0 ; i < 4; i++) {
@@ -30,15 +30,18 @@ function generateCharset() {
         }
     }
     console.log(charSet);
+    return charSet;
 }
 
 generateCharset();
 
-function generatePassword(charSet, length) {
-	var result = "";
-	for (var i = 0; i < length; i++)
-		result += charSet[Math.floor(Math.random() * charSet.length)];
-    console.log(result);
+function generatePassword() {
+    var charSet = generateCharset();
+    var result = "";
+    var passlength = parseInt(length);
+	for (var i = 0; i < passlength; i++)
+		result += charSet.charAt(Math.floor(Math.random() * charSet.length));
+    passEl.textContent = result;
 }
 
 generateBt.addEventListener("click", generatePassword);
